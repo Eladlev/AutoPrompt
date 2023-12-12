6c36aabfb72f5115f991ea64a1ae40b871004554
MatanFriedmann <34005034+MatanFriedmann@users.noreply.github.com>
2023-12-13 01:28:43 +0200

    Update base_dataset.py

--- dataset/base_dataset.py
+++ dataset/base_dataset.py
@@ -52,8 +52,8 @@ class DatasetBase:
         :param records: dataframes, update using pandas
         """
         if records is None:
-            records = [{'id': len(self.records) + i, 'text': sample, 'batch_id': batch_id} for
-                       i, sample in enumerate(sample_list)]
+            records = pd.DataFrame([{'id': len(self.records) + i, 'text': sample, 'batch_id': batch_id} for
+                       i, sample in enumerate(sample_list)])
         self.records = pd.concat([self.records, records], ignore_index=True)
 
     def update(self, records: pd.DataFrame):
