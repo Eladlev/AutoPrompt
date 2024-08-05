50f9e3b29ebdc3b12fd022c58ffd80bbb1822794
Akash Bajaj <abajaj@tcd.ie>
2024-08-05 21:32:52 +0100

    update json schema for metric generator

--- prompts/meta_prompts_generation/output_schemes.py
+++ prompts/meta_prompts_generation/output_schemes.py
@@ -87,36 +87,126 @@ step_prompt_schema = {
   "type": "object"
 }
 
+metric_generator_schema = {
+  "$defs": {
+    "MetricScale": {
+      "description": "Prompts for each evaluation scale for a given evaluation metric.",
+      "properties": {
+             "deficient_desc": {
+                    "description": "1. Deficient Performance on (a few words about the metric go here) (1): a single sentence description of what should be checked in the task output such that it classifies as a Deficient level of performance relative to this metric.",
+                    "title": "Deficient_Description",
+                    "type": "string"  
+                    },
+             "adequate_desc": {
+                    "description": "2. Adequate Performance on (a few words about the metric go here) (2): a single sentence description of what should be checked in the task output such that it classifies as a Adequate level of performance relative to this metric.",
+                    "title": "Adequate_Description",
+                    "type": "string" 
+                    },
+             "competent_desc": {
+                    "description": "3. Competent Performance on (a few words about the metric go here) (3): a single sentence description of what should be checked in the task output such that it classifies as a Competent level of performance relative to this metric.",
+                    "title": "Competent_Description",
+                    "type": "string" 
+                    },
+             "proficient_desc": {
+                    "description": "4. Proficient Performance on (a few words about the metric go here) (4): a single sentence description of what should be checked in the task output such that it classifies as a Proficient level of performance relative to this metric.",
+                    "title": "Proficient_Description",
+                    "type": "string" 
+                    },
+             "exemplary_desc": {
+                    "description": "5. Exemplary Performance on (a few words about the metric go here) (5): a single sentence description of what should be checked in the task output such that it classifies as a Exemplary level of performance relative to this metric.",
+                    "title": "Exemplary_Description",
+                    "type": "string" 
+                    }
+         },
+      "required": [
+             "deficient_desc",
+             "adequate_desc",
+             "competent_desc",
+             "proficient_desc",
+             "exemplary_desc"
+         ],
+      "title": "Metric_Scale_Object",
+      "type": "object"
+     },
+    "Metric": {
+      "description": "Details about a particular Evaluation Metric",
+      "properties": {
+         "metric_name": {
+            "description": "The name of the evaluation metric, in a few words, that will serve as the area of diagnostic evaluation for the task.",
+            "title": "Metric_Name",
+            "type": "string"
+             },
+         "metric_desc": {
+             "description": "Explanation, in not more than 3 sentences, what the above metric means and what it's trying to assess.",
+            "title": "Metric_Description",
+            "type": "string"
+             },
+         "metric_prompt": {
+            "description": "A prompt that will be used as input to an external evaluator agent to evaluate the assistant's performance based on the metric name and metric description generated above. This prompt should start with the phrase: 'Evaluate the performance of our agent using a five-point assessment scale that emphasizes', followed by a few words about the metric name and description, followed by another sentence starting with the phrase: 'Assign a single score of either 1, 2, 3, 4, or 5, with each level representing different degrees of perfection with respect to the', followed by just a few words again summarizing the metric name and description.",
+            "title": "Metric_Prompt",
+            "type": "string"
+              },
+         "metric_scale": {
+            "description": "A list of descriptions for each scale of assessment from 1 to 5 for the given metric. Note that in the above metric prompt structure, 1 represents the lowest level of performance and 5 represents the best level of performance.",
+            "items": {
+                "$ref": "#/$defs/MetricScale"
+                },
+            "title": "Metric_Scale",
+            "type": "array"
+                }
+       },
+      "required": ["metric_name", "metric_desc", "metric_prompt", "metric_scale"],
+      "title": "Metric",
+      "type": "object"
+    }
+  },
+  "description": "The collection of Metrics."
+  "properties": {
+    "metrics_list": {
+      "description": "The list of all possible metrics that would be important to assess whether this assistant performed the above task perfectly or not.",
+      "items": {
+        "$ref": "#/$defs/Metric"
+        },
+      "title": "Metrics_List",
+      "type": "array"
+    }
+  },
+  "required": [
+    "metrics_list"
+  ],
+  "title": "Metrics_Object",
+  "type": "object"
+}
 
-class MetricScale(BaseModel):
-  deficient_desc: str = Field(
-    description="1. Deficient Performance on (a few words about the metric go here) (1): a single sentence description of what should be checked in the task output such that it classifies as a Deficient level of performance relative to this metric.")
-  adequate_desc: str = Field(
-    description="2. Adequate Performance on (a few words about the metric go here) (2): a single sentence description of what should be checked in the task output such that it classifies as a Adequate level of performance relative to this metric.")
-  competent_desc: str = Field(
-    description="3. Competent Performance on (a few words about the metric go here) (3): a single sentence description of what should be checked in the task output such that it classifies as a Competent level of performance relative to this metric.")
-  proficient_desc: str = Field(
-    description="4. Proficient Performance on (a few words about the metric go here) (4): a single sentence description of what should be checked in the task output such that it classifies as a Proficient level of performance relative to this metric.")
-  exemplary_desc: str = Field(
-    description="5. Exemplary Performance on (a few words about the metric go here) (5): a single sentence description of what should be checked in the task output such that it classifies as a Exemplary level of performance relative to this metric.")
-
-
-class Metric(BaseModel):
-  metric_name: str = Field(
-    description="The name of the evaluation metric, in a few words, that will serve as the area of diagnostic evaluation for the task.")
-  metric_desc: str = Field(
-    description="Explanation, in not more than 3 sentences, what the above metric means and what it's trying to assess.")
-  metric_prompt: str = Field(
-    description="A prompt that will be used as input to an external evaluator agent to evaluate the assistant's performance based on the metric name and metric description generated above. This prompt should start with the phrase: 'Evaluate the performance of our agent using a five-point assessment scale that emphasizes', followed by a few words about the metric name and description, followed by another sentence starting with the phrase: 'Assign a single score of either 1, 2, 3, 4, or 5, with each level representing different degrees of perfection with respect to the', followed by just a few words again summarizing the metric name and description.")
-  metric_scale: List[MetricScale] = Field(
-    description="A list of descriptions for each scale of assessment from 1 to 5 for the given metric. Note that in the above metric prompt structure, 1 represents the lowest level of performance and 5 represents the best level of performance.")
-
-
-class MetricGeneratorFlow(BaseModel):
-  metrics_list: List[Metric] = Field(
-    description="The list of all possible rules that would be important to assess whether this assistant performed the above task perfectly or not.")
-
-metric_generator_schema = MetricGeneratorFlow
+#class MetricScale(BaseModel):
+#  deficient_desc: str = Field(
+#    description="1. Deficient Performance on (a few words about the metric go here) (1): a single sentence description of what should be checked in the task output such that it classifies as a Deficient level of performance relative to this metric.")
+#  adequate_desc: str = Field(
+#    description="2. Adequate Performance on (a few words about the metric go here) (2): a single sentence description of what should be checked in the task output such that it classifies as a Adequate level of performance relative to this metric.")
+#  competent_desc: str = Field(
+#    description="3. Competent Performance on (a few words about the metric go here) (3): a single sentence description of what should be checked in the task output such that it classifies as a Competent level of performance relative to this metric.")
+#  proficient_desc: str = Field(
+#    description="4. Proficient Performance on (a few words about the metric go here) (4): a single sentence description of what should be checked in the task output such that it classifies as a Proficient level of performance relative to this metric.")
+#  exemplary_desc: str = Field(
+#    description="5. Exemplary Performance on (a few words about the metric go here) (5): a single sentence description of what should be checked in the task output such that it classifies as a Exemplary level of performance relative to this metric.")
+#
+#
+#class Metric(BaseModel):
+#  metric_name: str = Field(
+#    description="The name of the evaluation metric, in a few words, that will serve as the area of diagnostic evaluation for the task.")
+#  metric_desc: str = Field(
+#    description="Explanation, in not more than 3 sentences, what the above metric means and what it's trying to assess.")
+#  metric_prompt: str = Field(
+#    description="A prompt that will be used as input to an external evaluator agent to evaluate the assistant's performance based on the metric name and metric description generated above. This prompt should start with the phrase: 'Evaluate the performance of our agent using a five-point assessment scale that emphasizes', followed by a few words about the metric name and description, followed by another sentence starting with the phrase: 'Assign a single score of either 1, 2, 3, 4, or 5, with each level representing different degrees of perfection with respect to the', followed by just a few words again summarizing the metric name and description.")
+#  metric_scale: List[MetricScale] = Field(
+#    description="A list of descriptions for each scale of assessment from 1 to 5 for the given metric. Note that in the above metric prompt structure, 1 represents the lowest level of performance and 5 represents the best level of performance.")
+#
+#
+#class MetricGeneratorFlow(BaseModel):
+#  metrics_list: List[Metric] = Field(
+#    description="The list of all possible rules that would be important to assess whether this assistant performed the above task perfectly or not.")
+#
+#metric_generator_schema = MetricGeneratorFlow
 
 def update_classification_prediction_schema(label_schema:list)->dict:
   """
