b5efd49025ccb584c2bb8a5ced7934721e638b40
Drishti Sushma <129742046+DrishtiShrrrma@users.noreply.github.com>
2024-08-08 14:21:09 +0530

    Update eval_utils.py

--- eval/eval_utils.py
+++ eval/eval_utils.py
@@ -19,7 +19,10 @@ def set_ranking_function(params):
         generation_dataset['text'] = '###User input:\n' + generation_dataset['text'] + '\n####model prediction:\n' + generation_dataset['prediction']
 
         generation_dataset = evaluator.apply_dataframe(generation_dataset)
+
+        # Convert the list of dictionaries having metric_name, metric_score, metric_reasoning to JSON string
         generation_dataset.score = generation_dataset.score.apply(lambda x: json.dumps(x))
+        
         dataset.score = generation_dataset.score
         return dataset
     return wrapper
