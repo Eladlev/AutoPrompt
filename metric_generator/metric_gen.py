f4c4796379b386802fe3e7b941c64020904282fd
Akash Bajaj <abajaj@tcd.ie>
2024-08-15 15:38:56 +0100

    dynamic metric generator code edits 1

--- metric_generator/metric_gen.py
+++ metric_generator/metric_gen.py
@@ -1,7 +1,7 @@
 from utils.llm_chain import ChainWrapper
 from langchain_core.pydantic_v1 import BaseModel, Field
 import pandas as pd
-
+from utils.dedup import Dedup
 
 class MetricMetadata(BaseModel):
     metric_score: float = Field(description="The score for the metric")
@@ -23,6 +23,7 @@ class MetricHandler:
         self.config = config
         self.metric_generator = metric_generator
         self.task_description = task_description
+        self.dedup = Dedup(self.config)
         self.metrics = self.generate_metrics()
 
     def get_metrics_info(self) -> dict:
@@ -39,6 +40,45 @@ class MetricHandler:
             for _, eval_prompt in metric_dict['metric_scale'][0].items():
                 metric_dict['metric_prompt'] += f'\n{eval_prompt}'
             del metric_dict['metric_scale']
+            
+    def hard_filter_metrics(self, metrics) -> list[bool]:
+        """
+        Produces a list of boolean values corresponding to whether the metric should be dropped (0) or not (1)
+        based on relevance to task description
+        Args:
+            metrics (list[dict]): list of metric dictionaries containing metric_name, metric_desc and metric_prompt
+        Returns:
+            list[bool]: a list of boolean values indicating whether the metric at that index should be kept (1) or dropped (0), referencing to the metrics list above
+        """
+        pass
+    
+    def get_semantic_metric_clusters(self, metrics) -> list[set(int)]:
+        """
+        Groups metrics in clusters based on semantic similarity using the Dedup class
+        Args:
+            metrics (list[dict]): list of metric dictionaries containing metric_name, metric_desc and metric_prompt
+        """
+        new_dedup = self.dedup.copy()
+        metrics_text = []
+        for metric_subdict in metrics:
+            curr_metric_text = f"{metric_subdict['metric_name']}. {metric_subdict['metric_desc']}"
+            metrics_text.append(curr_metric_text)
+        records = pd.DataFrame(metrics_text, columns=['text'])
+        return new_dedup.cluster_data(records)
+    
+    def sample_metrics(self, metrics, metric_clusters) -> list[dict]:
+        """
+        Samples a single metric for each metric cluster with size > 1. For clusters of size = 1, the single metric is kept as is.
+        The sample metric for a size > 1 cluster is synthetically generated using an LLM to be most representative of the metric clusters.
+
+        Args:
+            metrics (list[dict]): list of metric dictionaries containing metric_name, metric_desc and metric_prompt
+            metric_clusters (list[list[int]]): list of metric cluster, where each sublist is an individual cluster containing the 0-indexed indices of the metrics that belong to a given cluster, referencing to the original metrics list
+        Returns:
+            list[dict]: the new list of metric dictionaries containing metric_name, metric_desc and metric_prompt
+        """
+        pass
+    
 
     def generate_metrics(self) -> dict:
         """
@@ -47,7 +87,18 @@ class MetricHandler:
         metrics = self.metric_generator.invoke(
             {'task_description': self.task_description, 'num_metrics': self.config.num_metrics})
         metrics = metrics['metrics_list']
+        
         self.update_metrics(metrics)
+        
+        metrics_filter = self.hard_filter_metrics(metrics)
+        
+        metrics[:] = [metric_subdict for metric_subdict, keep_metric in zip(metrics, metrics_filter) if keep_metric]
+        
+        metric_clusters = self.get_semantic_metric_clusters(metrics)
+        metric_clusters = [list(cluster) for cluster in metric_clusters]
+        
+        metrics = self.sample_metrics(metrics, metric_clusters)
+        
         for metric in metrics:
             prompt = f'{metric["metric_prompt"]}\nThe following input should be evaluated according to the metric guidelines. \n###Evaluated input:\n{{sample}}\n###End'
             metric['metric_function'] = self.build_score_function(prompt)
