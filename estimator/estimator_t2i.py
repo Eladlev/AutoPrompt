eec90541a3ce5f6dd6286aff1eb1056500eb5263
Matan Friedmann <matanf@matans-mbp.mynetworksettings.com>
2024-09-15 20:43:48 -0400

    Small update to PR

--- estimator/estimator_t2i.py
+++ estimator/estimator_t2i.py
@@ -97,13 +97,8 @@ class T2IEstimator:
         Apply the estimator on a dataframe
         :param records: The record
         """
-        # num_images = len(records)
-        # prompt = self.cur_instruct['prompt']
         img_urls = self.image_generator(prompt=self.cur_instruct['prompt'],
                                         num_images=len(records))
-        # prompts = [self.cur_instruct['prompt']]*num_images
-        # img_urls = asyncio.run(self.image_generator(prompts))
-        # img_urls = [im.url for response in responses for im in response.data]
         records[self.mode] = img_urls
         return records
 
